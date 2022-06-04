eb0812b9590da39b9c01a62879defe33803f0980
Shentong Wang <wangs396@mcmaster.ca>
2022-06-04 18:03:49 -0400

    - Third commit from wst. - Added new feature:   - tooltips - Beautified the whole UI.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, redirect, url_for
 from flask import request, url_for, redirect, flash
 from flask import Flask, render_template
 from flask import url_for, escape
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -14,9 +14,10 @@ def index():
         if not expr:
             flash('Invalid input.')  # 显示错误提示
             return redirect(url_for('index'))  # 重定向回主页
-    #TODO: 这里expr已经提取了用户输入，计算逻辑在这写。
-    #call return x y
-    return render_template('index.html', x= [1,2,3,4], y = [5,6,7,8])
+    # TODO: 这里expr已经提取了用户输入，计算逻辑在这写。
+    # call return x y
+    return render_template('index.html', x=[1, 2, 3, 4], y=[5, 6, 7, 8])
+
 
 if __name__ == '__main__':
     app.run()
